2347525a3f8e0a38edb362c135f1ab5a238956b3
abdellah <Abdellah.chehri14@gmail.com>
2023-07-04 21:43:37 +0200

    add

--- src/components/common/header/commercial-header.tsx
+++ src/components/common/header/commercial-header.tsx
@@ -1,5 +1,9 @@
 import { FC } from 'react'
 import { UserNav } from '../tables/users-table/user-nav'
+import Image from 'next/image'
+import logo from "@/assets/lgog.png"
+import { Button } from '@/components/ui/button'
+
 
 interface commercialheaderAbdullahProps {
   
@@ -8,7 +12,22 @@ interface commercialheaderAbdullahProps {
 const CommercialHeader: FC<commercialheaderAbdullahProps> = ({}) => {
   return <div className='w-full h-[70px]  bg-black '>
     <div className="flex justify-between items-center h-full  container ">
-        <div className='h-full w-[200px]'>
+        <div className='h-full flex items-center justify-start w-[600px]'>
+          <div className='w-[20%] flex items-center justify-start gap-x-2'>
+            <Image
+              src={logo}
+              width={30} 
+              height={30}
+              alt='logo'
+            />
+            <h3 className='text-white text-xl mr-1 font-semibold'>Shawii</h3>
+          </div>
+          <Button variant="ghost" className='text-white w-fit flex'>
+            Les Raport
+          </Button>
+          <Button variant="ghost" className='text-white w-fit flex ' >
+            Les Dommand
+          </Button>
 
         </div>
             <UserNav />
