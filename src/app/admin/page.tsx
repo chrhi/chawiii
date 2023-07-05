2e3b79f74a498359d21419ffede0e9d5a75bfccb
abdellah <Abdellah.chehri14@gmail.com>
2023-07-05 19:22:24 +0200

    add

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,55 @@
+import { Metadata } from "next"
+import { columns } from "@/components/common/tables/users-table/columns"
+import { DataTable } from "@/components/common/tables/users-table/data-table"
+import AdminHeader from "@/components/common/header/admin-header"
+import { prisma } from "@/lib/prisma"
 
-import { FC } from 'react'
+async function getData() {
+  const users = await prisma.user.findMany()
+  return users
+}
 
-interface pageAbdullahProps {
+export const metadata: Metadata = {
+  title: "Admin",
+  description: "A task and issue tracker build using Tanstack Table.",
+}
+
+
+ 
+
+
+export default async function TaskPage() {
+
+  const users = await getData()
+
+  const DataFORMATED = users.map(item => {
+    return{
+      id: item.id,
+      name : item.name , 
+      status: "active",
+      email : item.email , 
+      type: item.type
+    }
+  })
+
   
-}
 
-const page: FC<pageAbdullahProps> = ({}) => {
-  return <div>page</div>
+  return (
+    <>
+     <AdminHeader  />
+      <div className="hidden h-full flex-1 max-w-6xl flex-col container space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Tableau des utrlistaerus pour l'Admin</h2>
+            <p className="text-muted-foreground">
+            Voici une liste de vos rapports !
+            </p>
+          </div>
+          
+        </div>
+        {/* @ts-ignore */}
+        <DataTable data={DataFORMATED} columns={columns} />
+      </div>
+    </>
+  )
 }
-
-export default page
---
+++ src/components/common/header/admin-header.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react'
+import Image from 'next/image'
+import logo from "@/assets/lgog.png"
+import { Button } from '@/components/ui/button'
+
+
+interface commercialheaderAbdullahProps {
+  
+}
+
+const AdminHeader: FC<commercialheaderAbdullahProps> = ({}) => {
+  return <div className='w-full h-[70px]  bg-black '>
+    <div className="flex justify-between items-center h-full  container ">
+        <div className='h-full flex items-center justify-start w-[600px]'>
+          <div className='w-[20%] flex items-center justify-start gap-x-2'>
+            <Image
+              src={logo}
+              width={30} 
+              height={30}
+              alt='logo'
+            />
+            <h3 className='text-white text-xl mr-1 font-semibold'>Shawii</h3>
+          </div>
+          <Button variant="ghost" className='text-white w-fit flex'>
+            Les utilisatures
+          </Button>
+        </div>
+        
+    </div>
+  </div>
+}
+
+export default AdminHeader
