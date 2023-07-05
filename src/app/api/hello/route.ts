66747ce2fc2c44a6a7d795414a2b3655709fb16b
abdellah <Abdellah.chehri14@gmail.com>
2023-07-05 13:34:43 +0200

    add

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,12 +1,74 @@
 import { NextResponse } from 'next/server'
+import { prisma } from '@/lib/prisma'
+
 
 export async function GET(request: Request) {
 
-    console.log("this route is working")
- 
-    
+ const reports = await prisma.report.findMany()
+
   return NextResponse.json({
     from : "abdullah ",
-    message : "the api is working"
-})
+    message : "here are all the users", 
+    reports
+ })
+}
+
+export async function POST(request: Request) {
+
+    console.log("this route is working")
+
+    const {action , title , ClientInformation , Date , Details , Request , id } = await request.json()
+
+    if(!action ){
+      return NextResponse.json({
+        from : "abdullah ",
+        message : "the api is working",
+        error : "you need to provide an action"
+    })
+    } 
+
+    if(action === "create report"){
+      const report  = await prisma.report.create({
+        data : {
+          title , ClientInformation , Date , Details , Request 
+          
+        }
+      })
+         return NextResponse.json({
+                from : "abdullah ",
+                message : "report has been created",
+                "report created" : report,
+               
+          })
+    }
+    if(action === "update report"){
+      const report  = await prisma.report.update({
+        data : {
+          title , ClientInformation , Date , Details , Request 
+          
+        },
+        where : {id}
+      })
+         return NextResponse.json({
+                from : "abdullah ",
+                message : "report has been updated",
+                "report updated" : report,
+          })
+    }
+    if(action === "delete report"){
+      const report  = await prisma.report.delete({
+        where : {id}
+      })
+         return NextResponse.json({
+                from : "abdullah ",
+                message : "report has been updated",
+                "report deleted" : report,
+          })
+    }
+
+    return NextResponse.json({
+        from : "abdullah ",
+        message : "the api is working but the provided action not working ",
+        action : action
+    })
 }
