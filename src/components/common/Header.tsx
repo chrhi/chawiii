c87a218969b7c56b2e965ae579e0ddbac56b3bda
abdellah <Abdellah.chehri14@gmail.com>
2023-07-10 16:45:00 +0200

    add

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { FC } from 'react'
+
+interface HeaderAbdullahProps {
+  
+}
+
+const Header: FC<HeaderAbdullahProps> = ({}) => {
+  return <div>Header</div>
+}
+
+export default Header
---
+++ src/components/common/SideBar.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { FC } from 'react'
+
+interface SideBarAbdullahProps {
+  
+}
+
+const SideBar: FC<SideBarAbdullahProps> = ({}) => {
+  return <div>SideBar</div>
+}
+
+export default SideBar
